b64d9545926484533d8926e7dd484abaa97c08e6
Ionut Morariu <ionut.morariu.c@gmail.com>
2017-09-27 11:00:52 +0200

    Front auth finalized

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Emaily!</h1>
+      Collect feedback from your customers, fast and easy!
+    </div>
+  );
+};
+
+export default Landing;
